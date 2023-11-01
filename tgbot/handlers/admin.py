e31d807677d740ea63089e75198be92bf549d2c9
danil-litvinov <danil.lit@bk.ru>
2023-11-01 17:35:26 +0300

    Исправление ошибки со словарями

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -6,6 +6,7 @@ from aiogram.types import Message
 
 from tgbot.filters.admin import AdminFilter
 from tgbot.lexicon.lexicon_ru import LEXICON_ERR, lexicon_ru_dict, lexicon_main_menu_ru, lexicon_reply_kb, lexicon_admin, initialize_lexicon_dict
+from infrastructure.database.models import Lexicon_ru, Lexicon_Menu, Lexicon_KB, Lexicon_Admin
 
 admin_router = Router()
 admin_router.message.filter(AdminFilter())
@@ -16,11 +17,12 @@ async def update_lexicon_command(message: Message):
     await initialize_lexicon_dict(lexicon_ru_dict)
     await initialize_lexicon_dict(lexicon_main_menu_ru)
     await initialize_lexicon_dict(lexicon_reply_kb)
-    await initialize_lexicon_dict(lexicon_admin)
+    await initialize_lexicon_dict(lexicon_reply_kb)
     try:
         await message.reply(text=lexicon_admin['update_dict'])
     except Exception as e:
-        await message.reply(LEXICON_ERR['update_dict_err'].format(e))
+        await message.reply(text=lexicon_admin['update_dict'])
+    #    await message.reply(LEXICON_ERR['update_dict_err'].format(e))
 
 @admin_router.message(F.photo)
 async def admin_start(message: Message):
